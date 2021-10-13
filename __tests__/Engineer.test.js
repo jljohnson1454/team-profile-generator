fe533839cfb7dd9686478aa8907a54e5d979e03b
Ghidorah <jljohnson1454@gmail.com>
2021-10-13 17:32:44 -0500

    testing Engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,9 @@
 const Engineer = require('../lib/Engineer');
+
+test('creates an engineer', () => {
+    const employee = new Engineer('Jacob', 136114, 'jljohnson1454@gmail.com');
+
+    expect(employee.name).toBe('Jacob');
+    expect(employee.id).toBe('136114');
+    expect(employee.email).toBe('jljohnson1454@gmail.com');
+  });
