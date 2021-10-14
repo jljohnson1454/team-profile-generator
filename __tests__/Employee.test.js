b6d750455dbdd0929baa6ca1be54c18dc6ab8121
Ghidorah <jljohnson1454@gmail.com>
2021-10-13 21:20:50 -0500

    testing super class

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,16 +1,35 @@
 const Employee = require('../lib/Employee.js');
 
-test('creates an Employee super class', () => {
+test('input a name', () => {
     const employee = new Employee('Jacob', 136114, 'jljohnson1454@gmail.com');
-
     expect(employee.name).toBe('Jacob');
+});
+
+test('get Name Method', () => {
+    const employee = new Employee('Jacob');
+    expect(employee.getName()).toBe('Jacob');
+})
+
+test('input a id', () => {
+    const employee = new Employee('Jacob', 136114, 'jljohnson1454@gmail.com');
     expect(employee.id).toBe(136114);
+});
+
+test('input an email', () => {
+    const employee = new Employee('Jacob', 136114, 'jljohnson1454@gmail.com');
     expect(employee.email).toBe('jljohnson1454@gmail.com');
 });
 
+test('input a role', () => {
+    const employee = new Employee('Jacob', 136114, 'jljohnson1454@gmail.com', 'Intern');
+    expect(employee.role).toBe('Intern');
+});
+
+
+
 
 // test('imports employee info from input', () => {
-//     const employee = new Employee();
+// const employee = new Employee();
 
 //     expect(employee.name).toEqual(expect.any(String));
 //     expect(employee.id).toEqual(expect.any(Number));
