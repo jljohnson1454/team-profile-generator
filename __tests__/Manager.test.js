68c9feaf24eee4fc86e55e4f47eb5728f7c530d1
Ghidorah <jljohnson1454@gmail.com>
2021-10-15 20:15:05 -0500

    tested Manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,17 +1,14 @@
 const Manager = require('../lib/Manager');
 
-test('this is the manager object', () => {
-
-    const manager = new Manager('Jacob', 136114, 'jljohnson1454@gmail.com',)
-
+test('this is the intern class', () => {
+    const manager = new Manager('Jacob', 136114, 'jljohnson1454@gmail.com', 95, 'Manager')
+    
     expect(manager.name).toBe('Jacob');
     expect(manager.id).toBe(136114);
     expect(manager.email).toBe('jljohnson1454@gmail.com');
-    expect(manager.officeNumber).toBe('95');
+    expect(manager.officeNumber).toBe(95);
     expect(manager.role).toBe('Manager');
-}
 
-    
-)
+})
 
 
